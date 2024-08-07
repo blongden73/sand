1d22ad6f615a3de1d1d3d70491f987e2c2f8ec2c
Ben Longden <hello@benlongdendesign.co.uk>
2024-08-07 12:59:40 +0100

    trim

--- js/filters.js
+++ js/filters.js
@@ -295,7 +295,7 @@ for(i=0; i < tags.length; i++) {
 setTimeout(() => {
     var description = document.querySelectorAll('.resort-description-list');
     for(i=0; i<description.length; i++) {
-        var html = description[i].innerText.replace(/[^\r\n]+/g, '<li>$&</li>');
+        var html = description[i].innerText.trim().replace(/[^\r\n]+/g, '<li>$&</li>');
         console.log(html);
         description[i].innerHTML = html;
     }
